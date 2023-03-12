c17e2d9b73665f3f6cbca32d81999295efb00453
oddy-bassey <oddybassey@gmail.com>
2023-03-12 19:18:32 +0100

    project setup

---
+++ src/main/java/com/revoltcode/TDDWitihJUnit/TddWitihJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.TDDWitihJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWitihJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWitihJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/TDDWitihJUnit/TddWitihJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.TDDWitihJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddWitihJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
